6a00974310aed0ce6124ff5ecbc6ec8447f8367e
seunghyeonKang <number42isuser@gmail.com>
2025-03-03 15:48:13 +0900

    ✨: 물리엔진 월드 생성 및 객체 적용

--- src/ex01.js
+++ src/ex01.js
@@ -1,6 +1,8 @@
 import * as THREE from "three";
+import * as CANNON from "cannon-es";
+import { OrbitControls } from "three/examples/jsm/Addons.js";
 
-// ----- 주제: cannon.js 기본 세팅
+// ----- 주제: cannon.js
 
 // cannon.js 문서
 // http://schteppe.github.io/cannon.js/docs/
@@ -35,20 +37,62 @@ export default function example() {
   scene.add(directionalLight);
 
   // Controls
+  const controls = new OrbitControls(camera, renderer.domElement);
+
+  // Cannon(물리 엔진)
+  const cannonWorld = new CANNON.World();
+  cannonWorld.gravity.set(0, -10, 0);
+
+  const floorShape = new CANNON.Plane();
+  const floorBody = new CANNON.Body({
+    mass: 0, // 무게
+    position: new CANNON.Vec3(0, 0, 0), // 위치
+    shape: floorShape,
+  });
+  floorBody.quaternion.setFromAxisAngle(
+    // rotation 대신 quaternion을 사용한다.
+    new CANNON.Vec3(-1, 0, 0),
+    Math.PI / 2
+  );
+  cannonWorld.addBody(floorBody);
+
+  const boxShape = new CANNON.Box(new CANNON.Vec3(0.25, 2.5, 0.25));
+  // 중심을 기준으로 거리가 얼마나 되는지를 기준으로 하기 때문에 Mesh 길이의 절반으로 해야한다.
+  const boxBody = new CANNON.Body({
+    mass: 1,
+    position: new CANNON.Vec3(0, 10, 0),
+    shape: boxShape,
+  });
+  cannonWorld.addBody(boxBody);
 
   // Mesh
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshStandardMaterial({
+  const floorMesh = new THREE.Mesh(
+    new THREE.PlaneGeometry(10, 10),
+    new THREE.MeshStandardMaterial({ color: "slategray" })
+  );
+  floorMesh.rotation.x = -Math.PI / 2;
+  scene.add(floorMesh);
+
+  const boxGeometry = new THREE.BoxGeometry(0.5, 5, 0.5);
+  const boxMaterial = new THREE.MeshStandardMaterial({
     color: "seagreen",
   });
-  const mesh = new THREE.Mesh(geometry, material);
-  scene.add(mesh);
+  const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+  boxMesh.position.y = 0.5;
+  scene.add(boxMesh);
 
   // 그리기
   const clock = new THREE.Clock();
 
   function draw() {
     const delta = clock.getDelta();
+
+    // console.log(delta);
+    let cannonStepTime = 1 / 60; // 화면 주사율에 따라 1/60 또는 1/120으로 최적화
+    if (delta < 0.01) cannonStepTime = 1 / 120;
+    cannonWorld.step(cannonStepTime, delta, 3); // 프레임수: 1/60초, 시간차, 지연 간격을 메우는 시도 횟수
+    boxMesh.position.copy(boxBody.position);
+    boxMesh.quaternion.copy(boxBody.quaternion);
 
     renderer.render(scene, camera);
     renderer.setAnimationLoop(draw);
